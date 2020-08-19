2319bb4615f28fbc38aea2f212c62d674e4597e5
MadboyJames <Jkc36@pitt.edu>
2020-08-19 15:36:10 -0400

    fixed layout not showing

--- StockJocky.Client/Controllers/HomeController.cs
+++ StockJocky.Client/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using StockJocky.Client.Models;
+using StockJocky.Domain.Models;
 
 namespace StockJocky.Client.Controllers
 {
@@ -18,9 +19,17 @@ namespace StockJocky.Client.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(UserViewModel userViewModel)
         {
-            return View();
+            //remove once user constructor is in
+            userViewModel.User = new User();
+            userViewModel.User.Stocks= new List<Stock>();
+            //remove once stock getting logic is implimented
+            userViewModel.User.Stocks.Add(new Stock(){Symbol="tst1",Price=1,PercentChange=.1,Name="Test One"});
+            userViewModel.User.Stocks.Add(new Stock(){Symbol="tst2",Price=2,PercentChange=.2,Name="Test Two"});
+            userViewModel.User.Stocks.Add(new Stock(){Symbol="tst3",Price=3,PercentChange=.3,Name="Test Three"});
+            
+            return View(userViewModel);
         }
 
         public IActionResult Privacy()
@@ -38,10 +47,10 @@ namespace StockJocky.Client.Controllers
             return View();
         }
 
-        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        // public IActionResult Error()
-        // {
-        //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        // }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
