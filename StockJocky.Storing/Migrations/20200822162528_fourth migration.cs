857937bbb470c1a4190df0348b24d479a66f037d
yujun chen <ychen246@hawk.iit.edu>
2020-08-22 11:46:02 -0500

    Migration update

---
+++ StockJocky.Storing/Migrations/20200822162528_fourth migration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StockJocky.Storing.Migrations
+{
+    public partial class fourthmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stocks_Users_UserId",
+                table: "Stocks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Stocks_UserId",
+                table: "Stocks");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Stocks");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Stocks",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stocks_UserId",
+                table: "Stocks",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stocks_Users_UserId",
+                table: "Stocks",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
